de59bbb1bfe112cc51059e8327a4c6c204843546
NUMBER03\user <tra.phannhu@gmail.com>
2018-06-11 21:22:54 +0700

    Update GateWay.java (untested)

--- src/simpleorm/GateWay.java
+++ src/simpleorm/GateWay.java
@@ -15,21 +15,129 @@ import simpleorm.Connector.ConnectionUtils;
 
 public abstract class GateWay {
 
-    public ResultSet findAll(String name, Object value) throws ClassNotFoundException {
+    //find allby table's name
+    public ResultSet findAll(String name) throws ClassNotFoundException {
         try {
-            String className = this.getClass().getSimpleName();
-            System.out.println("Name:" + className);
-            String sql = "Select * from student";
+            System.out.println("Name:" + name);
+            String sql = "Select * from " + name;
 
             // Thực thi câu lệnh SQL trả về đối tượng ResultSet.
             Connection conn = ConnectionUtils.getMyConnection();
             Statement statement = conn.createStatement();
             ResultSet rs = statement.executeQuery(sql);
-            //conn.close();
+            if (conn != null)
+                conn.close();
+            if (statement != null)
+                statement.close();
             return rs;
         } catch (SQLException ex) {
             Logger.getLogger(GateWay.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
+    }
+    //find by attribute(s)
+    public ResultSet findByAttribute(String[] attr) throws ClassNotFoundException {
+        try {
+            String sql = "Select ";
+            for (String single_attr : attr){
+                sql += single_attr;
+                sql += ",";
+            }
+            sql.substring(0, sql.length() - 1);
+            // Thực thi câu lệnh SQL trả về đối tượng ResultSet.
+            Connection conn = ConnectionUtils.getMyConnection();
+            Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            if (conn != null)
+                conn.close();
+            if (statement != null)
+                statement.close();
+            return rs;
+        } catch (SQLException ex) {
+            Logger.getLogger(GateWay.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+        
+    }
+    //insert
+    public void insert(String tableName, String[] columns, String[] values) throws ClassNotFoundException {
+        try {
+            if (columns.length == values.length)
+            {
+                String sql = "INSERT INTO " + tableName + " (";
+                for (String single_com : columns){
+                    sql += single_com;
+                    sql += ",";
+                }
+                sql.substring(0, sql.length() - 1);
+                sql += ") VALUES (";
+
+                for (String single_val : values){
+                    sql += single_val;
+                    sql += ",";
+                }
+
+                sql.substring(0, sql.length() - 1);
+                sql += ")";
+
+                // Thực thi câu lệnh SQL trả về đối tượng ResultSet.
+                Connection conn = ConnectionUtils.getMyConnection();
+                Statement statement = conn.createStatement();
+                statement.executeUpdate(sql);
+                if (conn != null)
+                    conn.close();
+                if (statement != null)
+                    statement.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(GateWay.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    //update
+    public void update(String tableName, String[] columns, String[] values, String condition) throws ClassNotFoundException {
+        try {
+            if (columns.length == values.length)
+            {
+                String sql = "UPDATE  " + tableName + " SET ";
+                for (int i = 0; i < columns.length; i++){
+                    sql+= columns[i] + " = " + values[i] + ", ";
+                }
+
+                sql.substring(0, sql.length() - 1);
+                sql += " WHERE " + condition;
+
+                // Thực thi câu lệnh SQL trả về đối tượng ResultSet.
+                Connection conn = ConnectionUtils.getMyConnection();
+                Statement statement = conn.createStatement();
+                statement.executeUpdate(sql);
+                if (conn != null)
+                    conn.close();
+                if (statement != null)
+                    statement.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(GateWay.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    //delete
+    public void delete(String tableName, String columns, String values) throws ClassNotFoundException {
+        try {
+            String sql = "DELETE FROM  " + tableName + " WHERE " + columns 
+                    + " = " + values;
+
+            // Thực thi câu lệnh SQL trả về đối tượng ResultSet.
+            Connection conn = ConnectionUtils.getMyConnection();
+            Statement statement = conn.createStatement();
+            statement.executeUpdate(sql);
+            if (conn != null)
+                conn.close();
+            if (statement != null)
+                statement.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(GateWay.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
 }
